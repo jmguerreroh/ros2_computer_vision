0a56b177507548e92723f27a4e4866ce07fc33c0
José Miguel Guerrero <josemiguel.guerrero@urjc.es>
2022-05-13 17:31:55 +0200

    Removing launcher after installation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 sudo apt install python3-vcstool -y
 vcs import < thirdparty.repos
 cp src/computer_vision/launch/tiago_spawn.launch.py src/ThirdParty/tiago_simulation/tiago_gazebo/launch/tiago_spawn.launch.py
+rm -rf src/computer_vision/launch/tiago_spawn.launch.py 
 cp -r src/aws_scenarios/* src/ThirdParty/pal_gazebo_worlds/
 rm -rf src/aws_scenarios
 colcon build --symlink-install
