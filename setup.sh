ec6e28f029a595fe79de53a2b5e260f5f7bcbf32
José Miguel Guerrero <josemiguel.guerrero@urjc.es>
2022-05-19 10:24:24 +0200

    Change folder, add commands, and update script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+# Installing ThirdParty repos
 sudo apt install python3-vcstool -y
 vcs import < thirdparty.repos
+rm -rf thirdparty.repos
+
+# Moving new Tiago launcher to change position using XML file
 cp src/computer_vision/launch/tiago_spawn.launch.py src/ThirdParty/tiago_simulation/tiago_gazebo/launch/tiago_spawn.launch.py
 rm -rf src/computer_vision/launch/tiago_spawn.launch.py 
-cp -r src/aws_scenarios/* src/ThirdParty/pal_gazebo_worlds/
-rm -rf src/aws_scenarios
+
+# Moving AWS worlds to pal_gazebo_worlds package
+cp -r src/aws_worlds/* src/ThirdParty/pal_gazebo_worlds/
+rm -rf src/aws_worlds
+
+# Compiling project
 colcon build --symlink-install
+
+# Including project's path in ~/.bashrc
 #source install/setup.bash
-#echo "source "${PWD}"/install/setup.bash" >> ~/.bashrc
+#echo "source "${PWD}"/install/setup.bash" >> ~/.bashrc 
+
+# Removing installation script
+rm -rf setup.sh
