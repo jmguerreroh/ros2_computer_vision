7cd373a32d56cb37f5db4945d45606a2995566ae
jmguerreroh <josemiguel.guerrero@urjc.es>
2022-07-29 10:49:16 +0200

    Adding photos to aws worlds

--- setup.sh
+++ setup.sh
@@ -6,12 +6,14 @@ vcs import < thirdparty.repos
 rm -rf thirdparty.repos
 
 # Moving new Tiago launcher to change position using YAML file
-cp src/computer_vision/launch/tiago_spawn.launch.py src/ThirdParty/tiago_simulation/tiago_gazebo/launch/tiago_spawn.launch.py
-rm -rf src/computer_vision/launch/tiago_spawn.launch.py 
+cp src/computer_vision/files/tiago_spawn.launch.py src/ThirdParty/tiago_simulation/tiago_gazebo/launch/tiago_spawn.launch.py
+rm -rf src/computer_vision/files/tiago_spawn.launch.py 
 
 # Moving AWS worlds to pal_gazebo_worlds package
 cp -r src/aws_worlds/* src/ThirdParty/pal_gazebo_worlds/
 rm -rf src/aws_worlds
+cp src/computer_vision/files/CMakeLists.txt src/pal_gazebo_worlds/CMakeLists.txt
+rm -rf src/computer_vision/files/CMakeLists.txt
 
 # Building project
 colcon build --symlink-install
