110b807be828f8413c421bc5751d8e20f519c286
Jose Miguel <josemiguel.guerrero@urjc.es>
2022-08-01 10:49:18 +0000

    Fixing script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-### New Version Script
-
-# # Installing ThirdParty repos
-# sudo apt install python3-vcstool -y
-# vcs import < thirdparty.repos
-# rm -rf thirdparty.repos
-#
-# # Moving new Tiago launcher to change position using YAML file
-# cp src/computer_vision/files/tiago_spawn.launch.py src/ThirdParty/tiago_simulation/tiago_gazebo/launch/tiago_spawn.launch.py
-# rm -rf src/computer_vision/files/tiago_spawn.launch.py
-#
-# # Moving AWS worlds to pal_gazebo_worlds package
-# cp -r src/aws_worlds/* src/ThirdParty/pal_gazebo_worlds/
-# rm -rf src/aws_worlds
-# cp src/computer_vision/files/CMakeLists.txt src/ThirdParty/pal_gazebo_worlds/CMakeLists.txt
-# rm -rf src/computer_vision/files
-#
-# # Building project
-# colcon build --symlink-install
-#
-# # Including the project's path in ~/.bashrc
-# #source install/setup.bash
-# #echo "source "${PWD}"/install/setup.bash" >> ~/.bashrc
-#
-# # Removing installation script
-# rm -rf setup.sh
-
-### New Version Script
-
 # Installing ThirdParty repos
 sudo apt install python3-vcstool -y
 vcs import < thirdparty.repos
@@ -38,7 +9,7 @@ colcon build --symlink-install
 
 # Setup Gazebo to find models - GAZEBO_MODEL_PATH
 source /usr/share/gazebo/setup.bash
-echo "source /usr/share/gazebo/setup.bash" >> ~/barhrc
+echo "source /usr/share/gazebo/setup.bash" >> ~/bashrc
 
 # Project's path
 source install/setup.bash
