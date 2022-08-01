85b3f7dd892265ca0c8d386b78f5c9cbdc15a1d7
Jose Miguel <josemiguel.guerrero@urjc.es>
2022-08-01 13:52:06 +0000

    Add dependencies and author

--- src/computer_vision/launch/tiago_cv.launch.py
+++ src/computer_vision/launch/tiago_cv.launch.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2021 PAL Robotics S.L.
+# Modified by José Miguel Guerrero Hernández
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
