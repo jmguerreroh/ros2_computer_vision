9249956aeb6886be5215ae4a714fa7d96d3faab0
jmguerreroh <josemiguel.guerrero@urjc.es>
2022-11-22 10:49:50 +0100

    Fixing bug in Tiago navigation real

--- src/computer_vision/launch/tiago_navigation_real.launch.py
+++ src/computer_vision/launch/tiago_navigation_real.launch.py
@@ -141,19 +141,6 @@ def generate_launch_description():
                               'use_respawn': use_respawn,
                               'container_name': 'nav2_container'}.items())
 
-    # bringup_cmd = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(launch_dir, 'bringup_launch.py')),
-    #     launch_arguments={'namespace': namespace,
-    #                       'use_namespace': use_namespace,
-    #                       'slam': slam,
-    #                       'map': map_yaml_file,
-    #                       'use_sim_time': use_sim_time,
-    #                       'params_file': params_file,
-    #                       'autostart': autostart,
-    #                       'use_composition': use_composition,
-    #                       'use_respawn': use_respawn}.items())
-
     nav_cmd = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(cv_dir, 'launch', 'navigation_launch_real.py')),
             launch_arguments={'use_sim_time': use_sim_time,
@@ -182,7 +169,6 @@ def generate_launch_description():
     # Add the actions to launch all of the navigation nodes
     ld.add_action(rviz_cmd)
     ld.add_action(nav_cmd)
-    # ld.add_action(bringup_cmd)
     ld.add_action(slam_cmd)
     ld.add_action(localization_cmd)
 
